e3750ea6aa2d247108b7a82ea5b700fb35b5ab37
filipemsl <filipelima0@live.com>
2021-08-13 15:14:48 -0300

    código refatorado para componentização

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const Dashboard: React.FC = () => {
+const components: React.FC = () => {
   return <div />;
 }
 
-export default Dashboard;
+export default components;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import logoOmega from './assets/logo-omega.svg'
-import fotoBanner from './assets/banner.webp'
-import linkedin from "./assets/linkedin.svg"
-import instagram from "./assets/instagram.svg"
-import youtube from "./assets/youtube.svg"
-import facebook from "./assets/facebook.svg"
-import twitter from "./assets/twitter.svg"
+import logoOmega from '../assets/logo-omega.svg'
+import fotoBanner from '../assets/banner.webp'
+import linkedin from "../assets/linkedin.svg"
+import instagram from "../assets/instagram.svg"
+import youtube from "../assets/youtube.svg"
+import facebook from "../assets/facebook.svg"
+import twitter from "../assets/twitter.svg"
 
 
 // import { Container } from './styles';
 
-const pages: React.FC = () => {
-  <>
-  </>
+const Home: React.FC = () => {
+  return (
+    <>
+      <div className="bg-white loginsize relative">
+        <div id="loginpage">
+          <aside className="banner">
+            <img src={logoOmega} alt="" width="240" />
+            <img src={fotoBanner} alt="Ilustração simbolizando perguntas e respostas" width="650" className="rounded-3xl" />
+            <div className="icons">
+              <a href="https://www.linkedin.com/company/omegaenergia/" target="_blank" rel="noreferrer"><img src={linkedin} alt="" /></a>
+              <a href="https://www.instagram.com/instadaomega/" target="_blank" rel="noreferrer"><img src={instagram} alt="" /></a>
+              <a href="https://www.youtube.com/channel/UC1z7YCcHngTPCRgzfayX6BA" target="_blank" rel="noreferrer"><img src={youtube} alt="" /></a>
+              <a href="https://www.facebook.com/omegaenergiarenovavel" target="_blank" rel="noreferrer"><img src={facebook} alt="" /></a>
+              <a href="https://twitter.com/tweetdaomega" target="_blank" rel="noreferrer"><img src={twitter} alt="" /></a>
+            </div>
+          </aside>
+          <main id="conteudo">
+
+            <div className="maincontent shadow-lg rounded-3xl">
+              teste
+            </div>
+
+          </main>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default pages;
+export default Home;
