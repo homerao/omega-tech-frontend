b2cd0b0bb885f4b23aa0e543dac88c80da3a4ef9
filipemsl <filipelima0@live.com>
2021-08-18 00:04:47 -0300

    semântica css do botão adicionar incluída

--- src/styles/DashboardStyle.ts
+++ src/styles/DashboardStyle.ts
@@ -9,4 +9,32 @@ display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
+box-sizing: border-box;
+`
+export const Container = styled.div`
+
+`
+
+export const AddButton = styled.button`
+color: #3980FF;
+width:120px;
+height:45px;
+border: 1px solid #3980ff;
+font-size: 15px;
+font-weight: bold;
+border-radius:0.625rem;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 10px;
+
+&:hover{
+  background: #3980FF;
+  color: #fff;
+
+  svg path {
+        fill: #fff;
+      }
+
+}
 `
